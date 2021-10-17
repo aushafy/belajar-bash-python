6f6d8c2c69c0d979b8086e3ca612665843496202
aushafy <aushafy@gmail.com>
2021-10-17 14:47:16 +0700

    add python data types

---
+++ python/6-data-types.py
@@ -0,0 +1,16 @@
+# int, float, complex
+a=3
+b=2.5
+c=5+8j
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+
+# string
+name="aushafy"
+print(name, type(name))
+
+# boolean
+# also implement typeCasting
+isName=bool(name)
+print(isName, type(isName))
