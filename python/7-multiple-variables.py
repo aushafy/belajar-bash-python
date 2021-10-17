0a18951e76190ca8ba6b94186f2ef9d1447007ac
aushafy <aushafy@gmail.com>
2021-10-17 18:37:53 +0700

    add multiple var python typing

---
+++ python/7-multiple-variables.py
@@ -0,0 +1,7 @@
+x=3
+y=10.5
+myName="Aushafy"
+#print(x,y,myName)
+# print("{} {} {}".format(x,y,myName))
+# print("{} \n{} \n{}".format(x,y,myName))
+print(f"x value is: {x} \nThe y value is: {y}\nmy name is: {myName}")
